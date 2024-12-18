1ef181ef5e4dadcbd909928221ca193698d8c712
up1 <somkiat.p@gmail.com>
2024-12-18 11:41:17 +0700

    Initial data

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -1,11 +1,26 @@
 package com.example.hello;
 
+import com.example.hello.product.Product;
+import com.example.hello.product.ProductRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class HelloApplication {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@PostConstruct
+	void initialData() {
+		Product p100 = new Product();
+		p100.setId(100);
+		p100.setName("Product 100");
+		productRepository.save(p100);
+	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context
