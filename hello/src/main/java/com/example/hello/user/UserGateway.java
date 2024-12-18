594998ace9d4deee32693f7012a700ed073908c9
up1 <somkiat.p@gmail.com>
2024-12-18 15:21:47 +0700

    Read data from properties file

--- hello/src/main/java/com/example/hello/user/UserGateway.java
+++ hello/src/main/java/com/example/hello/user/UserGateway.java
@@ -3,6 +3,7 @@ package com.example.hello.user;
 // https://jsonplaceholder.typicode.com/users/1
 
 import com.example.hello.user.dto.UserResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -10,14 +11,16 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class UserGateway {
     private final RestTemplate restTemplate;
+    private final String userUrl;
 
-    public UserGateway(RestTemplate restTemplate) {
+    public UserGateway(RestTemplate restTemplate,
+                       @Value("${user.url}") String userUrl) {
         this.restTemplate = restTemplate;
+        this.userUrl = userUrl;
     }
 
     public UserResponse getUserDetail(int id) {
-//        String url = "https://jsonplaceholder.typicode.com/users/" + id;
-        String url = "http://localhost:9999/users/" + id;
+        String url = userUrl + "/users/" + id;
         try {
             return restTemplate.getForObject(url, UserResponse.class);
         } catch (RestClientException e) {
