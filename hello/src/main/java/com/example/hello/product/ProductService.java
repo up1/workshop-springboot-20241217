9358fe76df370f109405eb42a1214a6cbf34e12f
up1 <somkiat.p@gmail.com>
2024-12-18 16:21:47 +0700

    Show tx propagation

--- hello/src/main/java/com/example/hello/product/ProductService.java
+++ hello/src/main/java/com/example/hello/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.hello.product;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,23 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Transactional(dontRollbackOn = ProductNotFoundException.class)
     public ProductResponse get(int id) {
+        Product p10 = new Product();
+        p10.setId(10);
+        p10.setName("p10");
+        productRepository.save(p10);
+
+        productRepository.deleteAll();
+
         // Get data from database
         Optional<Product> result = productRepository.findById(id);
         // Product not found
         if(result.isEmpty()) {
             throw new ProductNotFoundException("Product id=" + id + " not found");
         }
+
+
         // Success
         ProductResponse response = new ProductResponse();
         response.setId(id);
