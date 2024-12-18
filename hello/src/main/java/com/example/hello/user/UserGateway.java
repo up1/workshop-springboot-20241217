3bf8b0fdca53678821be414dc7e8f45209b2c035
up1 <somkiat.p@gmail.com>
2024-12-18 13:51:07 +0700

    Add user gateway

---
+++ hello/src/main/java/com/example/hello/user/UserGateway.java
@@ -0,0 +1,26 @@
+package com.example.hello.user;
+
+// https://jsonplaceholder.typicode.com/users/1
+
+import com.example.hello.user.dto.UserResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class UserGateway {
+    private final RestTemplate restTemplate;
+
+    public UserGateway(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public UserResponse getUserDetail(int id) {
+        String url = "https://jsonplaceholder.typicode.com/users/1";
+        try {
+            return restTemplate.getForObject(url, UserResponse.class);
+        } catch (RestClientException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
